eec99f4e4ae1c1560bdfa0ef7be021cf1d72bd1b
leviegu <leviegu@gmail.com>
2023-06-27 16:31:03 +0800

    chore: update lib and remove useless resources

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
-import "./styles.css";
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/css";
+import "material-icons/iconfont/material-icons.css";
+import "./index.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(createVuestic()).mount("#app");
